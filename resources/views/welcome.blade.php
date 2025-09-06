bd2427e6497f9d8804f4ee7eb8a1a4c943c54ec9
Matheus Brugge <matheusmilczwski@gmail.com>
2025-09-06 17:35:17 -0300

    migrando para laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <div class="container mt-4">
+       <button class="btn btn-primary">Ola mundo</button>
+    </div>
+
+</body>
+</html>
